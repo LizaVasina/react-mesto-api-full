79a2eca5ddf03215bd393b1b9c055693d09e04e2
Liza Vasina <liz.vasina@gmail.com>
2021-08-28 00:46:26 +0300

    add creash test route

--- backend/app.js
+++ backend/app.js
@@ -34,6 +34,12 @@ app.use(cors({ origin: "http://domainname.mesto.nomoredomains.monster", credenti
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
