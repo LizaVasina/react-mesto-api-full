f7df0e18c03bcd0adf4615caa3bde7aee20e9c91
Liza Vasina <liz.vasina@gmail.com>
2021-09-02 19:58:43 +0300

    check adding card

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -38,6 +38,7 @@ function App() {
 
     function handleCardLike(card) {
     const isLiked = card.likes.some(i => i._id === currentUser._id);
+      console.log(isLiked);
 
     api.changeLikeCardStatus(card._id, isLiked)
       .then((newCard) => {
