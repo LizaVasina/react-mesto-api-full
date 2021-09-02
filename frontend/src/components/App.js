abbac01b7f8a731a688f0185fcf9ed3cc643976a
Liza Vasina <liz.vasina@gmail.com>
2021-09-02 18:31:17 +0300

    fix card likes

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -55,9 +55,7 @@ function App() {
 
 
     function handleCardLike(card) {
-      console.log('prev', card._id);
     const isLiked = card.likes.some(i => i._id === currentUser._id);
-    console.log(isLiked);
 
     api.changeLikeCardStatus(card._id, isLiked)
       .then((newCard) => {
