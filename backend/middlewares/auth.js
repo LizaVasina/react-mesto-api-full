1547e6cdf05bdbe05fe5d78939c429693f9e96a3
Liza Vasina <liz.vasina@gmail.com>
2021-09-02 16:18:39 +0300

    check cookies

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,18 +2,18 @@ const jwt = require('jsonwebtoken');
 const InvalidTokenError = require('../errors/invalid-token');
 
 module.exports = (req, res, next) => {
-  // const { cookie } = req.headers;
+  const { cookie } = req.headers;
 
-  // if (!cookie || !cookie.startsWith('jwt=')) {
-  //   next(new InvalidTokenError('Неверный токен, необходима авторизация'));
-  // }
+  if (!cookie || !cookie.startsWith('jwt=')) {
+    next(new InvalidTokenError('Неверный токен, необходима авторизация'));
+  }
 
-  // const token = cookie.replace('jwt=', '');
+  const token = cookie.replace('jwt=', '');
 
   let payload;
 
   try {
-    payload = jwt.verify(req.cookies.jwt, 'dev-secret-key');
+    payload = jwt.verify(token, 'dev-secret-key');
   } catch (err) {
     next(new InvalidTokenError('Неверный токен, необходима авторизация'));
   }
